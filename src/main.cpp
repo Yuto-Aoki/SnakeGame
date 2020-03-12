c1b0f01ed066beea3dab81e7f90c9047af86564a
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-12 21:32:11 +0900

    Add set up and const value

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+using namespace std;
+
+const int width = 20;
+const int height = 20;
+
+int x, y, fruitX, fruitY, score;
+enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN};
+direction dir;
+bool Gameover;
+ 
+
+void Setup(){
+    Gameover = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+void Display();
 
 int main(){
+    Setup();
+    Display();
     return 0;
 }
